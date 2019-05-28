ec13814d242e57e8d8568deb199fe96283374719
Gianfranco Rusca <gruscaa@gmail.com>
2019-05-28 21:25:42 +0200

    minor change

--- routes/jobs-routes.js
+++ routes/jobs-routes.js
@@ -6,16 +6,28 @@ const Client = require('../models/client-model');
 const Job = require('../models/job-model');
 
 
+// GET '/api/clients/:clientId/jobs/:jobId' ---- Retrieve Specific Job
+router.get('/clients/:clientId/jobs/:jobId', (req, res) => {
+  
+  Job.findById(req.params.jobId)
+  .then((foundJob) =>{
+      res.json(foundJob);
+  })
+  .catch( err => res.status(500).json(err))
+});
+
 
 // POST '/api/jobs' ---- Create New Job
 
 router.post('/jobs', (req, res, next) => {
-        const { title, description, progress, clientID } = req.body;
+        const { title, description, progress, price, clientID } = req.body;
       
     Job.create({
         title: title,
         description: description,  
         progress: progress, 
+        price: price, 
+        amountpaid: 0,
         client: clientID,
         keycode: Math.random().toString(36).substring(7),
     })
@@ -29,19 +41,6 @@ router.post('/jobs', (req, res, next) => {
     })
     .catch(err => res.status(500).json(err))
 });
-
-
-
-// GET '/api/clients/:clientId/jobs/:jobId' ---- Retrieve Specific Job
-router.get('/clients/:clientId/jobs/:jobId', (req, res) => {
-  
-    Job.findById(req.params.jobId)
-    .then((foundJob) =>{
-        res.json(foundJob);
-    })
-    .catch( err => res.status(500).json(err))
-  });
-
 
 
 // PUT '/api/jobs/:id' ---- Update Specific Job
@@ -67,7 +66,7 @@ router.put('/jobs/:id', (req, res, next) => {
 
 
 
-  // DELETE '/api/tasks/:id'     => to delete a specific task
+  // DELETE '/api/jobs/:id'     => to delete a specific job
 router.delete('/jobs/:id', (req, res) => {
     const { id } = req.params;
   
